e4a15edbf0b5192a83479eadbcd312bdac7db34b
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-06-24 18:41:21 +0200

    feat: update routes with basic routes and update server file

--- list-films/routes/main.routes.js
+++ list-films/routes/main.routes.js
@@ -1,10 +1,40 @@
 const express = require('express')
 const router = express.Router()
-const path = require('path')
 const ejs = require('ejs')
 
-router.get('/', (req, res) => {
-  res.render('pages/list')
+const filmController = require('../controllers/film.controller')
+
+router.get('/', async (req, res) => {
+  const films = await filmController.getList()
+  res.render('pages/list', {
+    films: films
+  })
+})
+
+router.post('/new-film', async (req, res) => {
+  try {
+    if(!req.body.year) throw new Error('Missing year')
+    if(!req.body.title) throw new Error('Missing title')
+
+    filmController.newFilm(req.body)
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error);
+  }
+})
+
+router.post('/delete-film/:id', async (req, res) => {
+  try {
+    if(!req.params.id) throw new Error('Missing ID')
+    const result = await filmController.deleteFilm(req.params.id)
+    if(result) res.redirect('/')
+    else throw new Error('Film not found')
+  }
+  catch (error) {
+    console.log(error)
+    res.status(500).send(error);
+  }
 })
 
 module.exports = router
--- list-films/server.js
+++ list-films/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
 
 const mainRoutes = require('./routes/main.routes')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static('public'))
+
+var urlEncodedParser = bodyParser.urlencoded({ extended: false })
+app.use(urlEncodedParser)
+
 app.use('/', mainRoutes)
 
 app.listen(3000)
