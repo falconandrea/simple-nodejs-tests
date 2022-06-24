75e340eefe331d783e97921b475d01030e4b5a18
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-06-24 18:40:59 +0200

    feat: create controller with basic methods

---
+++ list-films/controllers/film.controller.js
@@ -0,0 +1,34 @@
+const Film = require("../models/film.model")
+
+const getList = async () => {
+  const films = await Film.find({})
+  return films ?? []
+}
+
+const newFilm = (data) => {
+  const film = new Film(data)
+  return film.save()
+}
+
+const findById = async (id) => {
+  const film = await Film.findById(id)
+
+  if(film) return film
+  else return null
+}
+
+const deleteFilm = async (id) => {
+  const film = await findById(id)
+
+  if(film) {
+    film.delete()
+    return true
+  }
+  else return false
+}
+
+module.exports = {
+  getList,
+  newFilm,
+  deleteFilm
+}
