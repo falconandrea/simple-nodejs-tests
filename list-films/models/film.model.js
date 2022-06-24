a934bd7d34d0a7fea380c16789f8a6db9442350b
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-06-24 18:40:44 +0200

    feat: create model mongodb for films

---
+++ list-films/models/film.model.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected db successfully");
+});
+
+const FilmSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports =  mongoose.model("Film", FilmSchema)
