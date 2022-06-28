9e4b7872c48920b1f12f0d4a00c351e8a2e25358
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-06-28 19:36:24 +0200

    feat: dynamic load db for tests

--- list-films/models/film.model.js
+++ list-films/models/film.model.js
@@ -1,13 +1,8 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.NODE_ENV === 'development' ? process.env.MONGO_URI : process.env.MONGO_URI_TEST, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true
 })
 
