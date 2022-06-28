4c5a8f3b8c80ccdc968f6b898d9f1dab7ac300c0
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-06-28 19:36:07 +0200

    feat: install jest for test and update server.js for export app

--- list-films/server.js
+++ list-films/server.js
@@ -15,7 +15,4 @@ app.use(urlEncodedParser)
 
 app.use('/', mainRoutes)
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`)
-})
+module.exports = app
