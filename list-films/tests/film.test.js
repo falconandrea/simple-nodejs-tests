3ce0a86bc43ff784a6503d0bae408654d7e24e9c
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-06-29 19:30:29 +0200

    feat: update tests

--- list-films/tests/film.test.js
+++ list-films/tests/film.test.js
@@ -2,7 +2,7 @@ const app = require('../server')
 const supertest = require('supertest')
 const request = supertest(app)
 
-describe('Test endpoints', () => {
+describe('Test front-end endpoints', () => {
   beforeAll(async () => {
     const Film = require('../models/film.model')
 
@@ -20,6 +20,55 @@ describe('Test endpoints', () => {
         expect(response.status).toBe(200)
         expect(response.type).toBe('text/html')
         expect(response.text).toContain('Random string test')
+      })
+  })
+
+  it('add new film', async () => {
+    const res = await request.post('/new-film')
+      .send('title=New film test&year=2021')
+      .expect(response => {
+        expect(response.status).toBe(200)
+        expect(response.type).toBe('text/html')
+        expect(response.text).toContain('Random string test')
+        expect(response.text).toContain('New film test')
+      })
+  })
+
+  it('add new film without year', async () => {
+    const res = await request.post('/new-film')
+      .send('title=Film without year')
+      .expect(response => {
+        expect(response.status).toBe(500)
+        expect(response.text).toContain('Missing year')
+      })
+  })
+
+  it('add new film without title', async () => {
+    const res = await request.post('/new-film')
+      .send('year=1980')
+      .expect(response => {
+        expect(response.status).toBe(500)
+        expect(response.text).toContain('Missing title')
+      })
+  })
+
+  it('delete a film', async () => {
+    const titleFilm = 'New film test'
+    const filmController = require('../controllers/film.controller')
+    const film = await filmController.findByTitle(titleFilm)
+
+    const res = await request.post(`/delete-film/${film.id}`)
+      .expect(response => {
+        expect(response.status).toBe(200)
+        expect(response.text).not.toContain(titleFilm)
+      })
+  })
+
+  it('delete a film with wrong id', async () => {
+    const res = await request.post('/delete-film/62bc8747de2202dba8904293')
+      .expect(response => {
+        expect(response.status).toBe(500)
+        expect(response.text).toContain('Film not found')
       })
   })
 })
