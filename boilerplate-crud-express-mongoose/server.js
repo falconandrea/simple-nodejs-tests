b086bdfa570557d25cb73a1454f34d9e83df99b9
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-08-29 19:44:17 +0200

    chore: missing file prev commit

--- boilerplate-crud-express-mongoose/server.js
+++ boilerplate-crud-express-mongoose/server.js
@@ -4,9 +4,13 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const userRoutes = require('./routes/user.routes')
+
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
+
+app.use('/api/users', userRoutes)
 
 app.use((req, res) => {
   res.status(404).send('Unknown endpoint')
